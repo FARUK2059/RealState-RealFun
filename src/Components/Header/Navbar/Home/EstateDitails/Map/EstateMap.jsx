11033383a4d9bcdb4301d0864bf9a5bfa72c1749
FARUK2059 <omarfaruk34552@gmail.com>
2024-04-18 19:34:06 +0600

    Reisize for leaflet map

--- src/Components/Header/Navbar/Home/EstateDitails/Map/EstateMap.jsx
+++ src/Components/Header/Navbar/Home/EstateDitails/Map/EstateMap.jsx
@@ -7,7 +7,7 @@ const EstateMap = () => {
   return (
     <div className='grid max-h-screen p-4 z-50 items-center justify-center'>
         <p className='p-4 text-2xl font-extrabold text-yellow-400'> RealFun Real Estate location</p>
-      <div style={{ height: '500px', width: '1200px' }}>
+      <div style={{ height: '500px', width: '1000px' }}>
         <MapContainer 
           center={[24.91667, 91.76667]} 
           zoom={10} 
